a8f0f412d2fcfcef1f9c73f25cbe82f5578ee732
qu4rkb17 <qu4rkb17@gmail.com>
2019-07-10 16:48:13 +0800

    Add Insert_Comparator

---
+++ Insert_Comparator.java
@@ -0,0 +1,27 @@
+import java.util.Comparatorl
+public class Transaction{
+
+	private final String who;
+	private final Date when;
+	private final double amount;
+	//
+	public static class WhoOrder implements Comparator<Transaction>{
+		public int compare(Transaction v, Transaction w){
+			return v.who.compareTo(w.who);
+		}
+	}
+
+	public static class WhenOrder implements Comparator<Transaction>{
+		public int compare(Transaction v, Transaction w){
+			return v.when.compareTo(w.when);
+		}
+	}
+
+	public static class HowMuchOrder implements Comparator<Transaction>{
+		public int compare(Transaction v, Transaction w){
+			if (v.amount < w.amount)	return -1;
+			if (v.amount > w.amount)	return +1;
+			return 0;
+		}
+	}
+}
