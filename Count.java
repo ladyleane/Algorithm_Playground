7c2c97ce8da95c4fdc760ed5332cd7bfcb49aed2
qu4rkb17 <qu4rkb17@gmail.com>
2019-08-07 09:11:02 +0800

    Add Count

---
+++ Count.java
@@ -0,0 +1,19 @@
+public class Count{
+	public static void main(String[] args) {
+		Alphabet alpha = new Alphabet(args[0]);
+		int R = alpha.R();
+		int[] count = new int[R];
+
+		String s = StdIn.readALl();
+		int N = s.length();
+		for (int i = 0; i < N; i++) {
+			if (alpha.contains(s.charAt(i))) {
+				count[alpha.toIndex(s.charAt(i))]++;
+			}
+		}
+
+		for (int c = 0; c < R; c++) {
+			StdOut.println(alpha.toChar(c) + " " + count[c]);
+		}
+	}
+}
