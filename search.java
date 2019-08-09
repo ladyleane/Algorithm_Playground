93e29454446bb5d3a295f4b8b4c94116816272c5
qu4rkb17 <qu4rkb17@gmail.com>
2019-08-09 14:44:05 +0800

    Add search

---
+++ search.java
@@ -0,0 +1,27 @@
+public static int search(String pat, String txt){
+	int M = pat.length();
+	int N = txt.length();
+	for (int i = 0; i <= N - M; i++){
+		int j;
+		for (j = 0; j < M; j++)
+			if (txt.charAt(i + j) != pat.charAt(j))
+				break;
+		if (j == M)	return i;
+	}
+	return N;
+}
+
+//Explicit Backward
+public static int search(String pat, String txt){
+	int j, M = pat.length();
+	int i, N = txt.length();
+	for (i = 0, j = 0; i < N && j < M; i++){
+		if (txt.charAt(i) == pat.charAt(j))	j++;
+		else{
+			i -= j;
+			j = 0;
+		}
+	}
+	if (j == M)	return i - M;
+	else 	return N;
+}
