d95ab28506195519c8aa946e55e06cf74a0b65a9
qu4rkb17 <qu4rkb17@gmail.com>
2019-08-12 22:13:43 +0800

    Add GREP

--- NFA.java
+++ NFA.java
@@ -67,3 +67,17 @@ public class NFA{
 		return false;
 	}
 }
+
+//GREP
+public class GREP{
+	public static void main(String[] args) {
+		String regexp = '(.*)' + args[0] + '.*)';
+		NFA nfa = new NFA(regexp);
+		while (StdIn.hasNextLine()){
+			String txt = StdIn.readLine();
+			if (nfa.recognizes(txt)) {
+				StdOut.println(txt);
+			}
+		}
+	}
+}
